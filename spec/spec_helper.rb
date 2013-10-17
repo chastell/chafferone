0a7bfc9a56b86a5689e826f8423f2403315c8f2e
Piotr Szotkowski <chastell@chastell.net>
2013-10-17 23:40:45 +0200

    spec_helper: configure Bogus

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require 'bundler/setup'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'bogus/minitest/spec'
 require 'chafferone'
+
+Bogus.configure { |config| config.search_modules << Chafferone }
