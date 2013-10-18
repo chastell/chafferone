4160a409adfed655a8c7f4bcad49e734418b33b5
Piotr Szotkowski <chastell@chastell.net>
2013-10-18 09:54:52 +0200

    factor out ChatterboxOffice::Entry

--- lib/chafferone/chatterbox_office.rb
+++ lib/chafferone/chatterbox_office.rb
@@ -1,12 +1,13 @@
 module Chafferone class ChatterboxOffice
+  Entry = Struct.new :source, :weight
+
   def initialize posts: []
     @posts = posts
   end
 
   def turnover
-    posts.group_by(&:source).map do |source, posts|
-      { source: source, weight: posts.size }
-    end.sort_by { |elem| elem[:weight] }.reverse
+    posts.group_by(&:source).map { |source, posts| Entry[source, posts.size] }
+      .sort_by(&:weight).reverse
   end
 
   attr_reader :posts
--- spec/chafferone/chatterbox_office_spec.rb
+++ spec/chafferone/chatterbox_office_spec.rb
@@ -3,13 +3,14 @@ require_relative '../spec_helper'
 module Chafferone describe ChatterboxOffice do
   describe '#turnover' do
     it 'returns the list of sources weighted by participation' do
-      jruby, rubinius = fake(:source), fake(:source)
-      posts = [fake(:post, source: jruby), fake(:post, source: rubinius),
-        fake(:post, source: jruby), fake(:post, source: rubinius),
+      jruby = fake :source
+      rubix = fake :source
+      posts = [fake(:post, source: jruby), fake(:post, source: rubix),
+        fake(:post, source: jruby), fake(:post, source: rubix),
         fake(:post, source: jruby)]
       ChatterboxOffice.new(posts: posts).turnover.must_equal [
-        { source: jruby,    weight: 3 },
-        { source: rubinius, weight: 2 },
+        ChatterboxOffice::Entry[jruby, 3],
+        ChatterboxOffice::Entry[rubix, 2],
       ]
     end
   end
